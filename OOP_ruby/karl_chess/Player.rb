9c9045f58d2edda44ce9d2681699453b8f23e9cb
Karl Osterbauer <kosterb1@jhu.edu>
2019-07-24 12:55:17 -0400

    restructuring repository

--- OOP_ruby/karl_chess/Player.rb
+++
@@ -1,7 +0,0 @@
-class Player
-	attr_reader :color
-	def initialize(color, disp)
-		@color = color
-		@display = disp
-	end
-end
--- OOP_ruby/karl_chess/pieces/Included.rb
+++
@@ -1,8 +0,0 @@
-require_relative "Knight.rb"
-require_relative "King.rb"
-require_relative "Queen.rb"
-require_relative "NullPiece.rb"
-require_relative "Bishop.rb"
-require_relative "Rook.rb"
-require_relative "Pawn.rb"
-
